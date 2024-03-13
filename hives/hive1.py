5a3369ec06b11675a1668aca8f0a85c08b42082d
Marcel Suter <marcel.suter@bzz.ch>
2024-03-13 06:53:02 +0100

    template for a hive

--- hives/hive1.py
+++ hives/hive1.py
@@ -1,14 +1,11 @@
-import json
-import random
 import selectors
 import socket
-import traceback
 
 from message.client_message import ClientMessage
 from message.server_message import ServerMessage
 
 ANTHILL_HOST = '127.0.0.1'
-ANTHILL_PORT = 62222
+ANTHILL_PORT = 62123 # TODO 62000 - 62999
 DISCOVERY_HOST = '127.0.0.1'
 DISCOVERY_PORT = 61111
 DEBUG = True
@@ -21,47 +18,11 @@ def main():
 
 def register():
     """
-    register this anthill services with the discovery service
-    :return:
+    register this anthill service with the discovery service
+    :return: None
     """
-    action = {
-        'action': 'register',
-        'ip': ANTHILL_HOST,
-        'port': ANTHILL_PORT,
-        'type': 'hive'
-    }
-
-    sel = selectors.DefaultSelector()
-    request = create_request(action)
-    start_connection(
-        sel,
-        DISCOVERY_HOST,
-        DISCOVERY_PORT,
-        request
-    )
-
-    message = None
-    try:
-        while True:
-            events = sel.select(timeout=1)
-            for key, mask in events:
-                message = key.data
-                try:
-                    message.process_events(mask)
-                except Exception:
-                    print(
-                        f'Anthill1: Error: Exception for {message.ipaddr}:\n'
-                        f'{traceback.format_exc()}'
-                    )
-                    message.close()
-            # Check for a socket being monitored to continue.
-            if not sel.get_map():
-                break
-    except KeyboardInterrupt:
-        print('Anthill1: Caught keyboard interrupt, exiting')
-    finally:
-        sel.close()
-    if DEBUG: print(message)
+    # TODO
+    pass
 
 
 def game():
@@ -69,48 +30,25 @@ def game():
     process the rounds of the game
     :return:
     """
-    sel = selectors.DefaultSelector()
-    host = ANTHILL_HOST
-    port = ANTHILL_PORT
+    # TODO
 
-    lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Avoid bind() exception: OSError: [Errno 48] Address already in use
-    lsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    lsock.bind((host, port))
-    lsock.listen()
-    if DEBUG: print(f'Hive1: Listening on {(host, port)}')
-    lsock.setblocking(False)
-    sel.register(lsock, selectors.EVENT_READ, data=None)
+def process_action(message):
+    """
+    process the action from the game service
+    :param message:
+    :return:
+    """
+    # TODO
+    pass
 
-    playing = True
-    try:
-        while playing:
-            events = sel.select(timeout=None)
-            for key, mask in events:
-                if key.data is None:
-                    accept_wrapper(sel, key.fileobj)
-                else:
-                    message = key.data
-                    try:
-                        message.process_events(mask)
-                        process_action(message)
-                    except StopIteration:
-                        if DEBUG: print(f'Hive1: Game over')
-                        playing = False
-                    except Exception:
-                        print(
-                            f'Hive1: Error: Exception for {message.ipaddr}:\n'
-                            f'{traceback.format_exc()}'
-                        )
-                        message.close()
-    except KeyboardInterrupt:
-        if DEBUG: print('Hive1: Caught keyboard interrupt, exiting')
-
-    finally:
-        sel.close()
 
 
 def create_request(action_item):
+    """
+    creates the body of the request
+    :param action_item:
+    :return:
+    """
     return dict(
         type='text/json',
         encoding='utf-8',
@@ -119,8 +57,16 @@ def create_request(action_item):
 
 
 def start_connection(sel, host, port, request):
+    """
+    starts the connection to a remote socket
+    :param sel:
+    :param host:
+    :param port:
+    :param request:
+    :return:
+    """
     addr = (host, port)
-    if DEBUG: print(f'Anthill1: Starting connection to {addr}')
+    if DEBUG: print(f'Hive1: Starting connection to {addr}')
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setblocking(False)
     sock.connect_ex(addr)
@@ -129,36 +75,17 @@ def start_connection(sel, host, port, request):
     sel.register(sock, events, data=message)
 
 
-def process_action(message):
-    """
-    process the action from the game service
-    :param message:
-    :return:
-    """
-    if DEBUG: print(f'Anthill1: message.event={message.event}')
-    if message.event == 'READ':
-        action = message.request['action']
-        if action == 'quit':
-            message.response = 'OK'
-        elif action == 'round':
-            directions = ['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE', 'H']
-            moves = random.choices(directions, k=message.request['count'])
-            ''' only for debugging '''
-            #moves.pop()
-            #moves.append('H')
-            #moves[0] = 'X'
-            ''' only for debugging '''
-            message.response = json.dumps(moves)
-        message.set_selector_events_mask('w')
-    else:
-        action = message.request['action']
-        if action == 'quit':
-            raise StopIteration
 
 
 def accept_wrapper(sel, sock):
+    """
+    Wrapper for the recieved messages
+    :param sel:
+    :param sock:
+    :return:
+    """
     conn, addr = sock.accept()  # Should be ready to read
-    if DEBUG: print(f'Anthill1: Accepted connection from {addr}')
+    if DEBUG: print(f'Hive1: Accepted connection from {addr}')
     conn.setblocking(False)
     message = ServerMessage(sel, conn, addr)
     sel.register(conn, selectors.EVENT_READ, data=message)
